8920a8670f7f8a4f36ad0bdbb0ded2c88f462adb
Tuyen <nguyenanhtuyen10a5@gmail.com>
2023-07-09 13:31:57 +0700

    first commit

---
+++ src/main/java/com/nguyenanhtuyen/Controller/HelloSpring.java
@@ -0,0 +1,12 @@
+package com.nguyenanhtuyen.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloSpring {
+	@GetMapping("/hello")
+	public String Index() {
+		return "index";
+	}
+}
