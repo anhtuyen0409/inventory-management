09e60f083e5399e55110f87fa18a183f9c014620
Tuyen <nguyenanhtuyen10a5@gmail.com>
2023-07-14 18:48:52 +0700

    config

--- src/main/java/com/nguyenanhtuyen/Controller/HelloSpring.java
+++ src/main/java/com/nguyenanhtuyen/Controller/HelloSpring.java
@@ -1,4 +1,4 @@
-package com.nguyenanhtuyen.Controller;
+package com.nguyenanhtuyen.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
